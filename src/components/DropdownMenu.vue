3563d63548f436cd6e5565e80e84781db8304192
kyosuke <kyosuke.ito@gmail.com>
2022-04-03 15:42:11 -0700

    create drop down menu

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,10 +1,30 @@
-<script setup></script>
+<script setup>
+import { UserIcon, LogoutIcon } from "@heroicons/vue/outline";
+</script>
 
 <template>
-  <div>
+  <div class="relative">
     <img
       src="../assets/avatar.jpeg"
       class="rounded-full w-10 h-10 cursor-pointer"
     />
+    <div
+      class="absolute top-16 right-0 z-10 w-40 py-2 bg-white rounded-sm shadow"
+    >
+      <ul>
+        <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+          <a href="/#" class="flex items-center space-x-2">
+            <UserIcon class="w-5 h-5" />
+            <span class="text-sm font-bold">Profile</span>
+          </a>
+        </li>
+        <li class="text-gray-700 hover:bg-blue-100 hover:text-blue-600 p-2">
+          <a href="/#" class="flex items-center space-x-2">
+            <LogoutIcon class="w-5 h-5" />
+            <span class="text-sm font-bold">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
