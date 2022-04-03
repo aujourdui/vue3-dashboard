e21caf1579da9b38e50eb3d03f5977ddf7c1a929
kyosuke <kyosuke.ito@gmail.com>
2022-04-03 14:59:03 -0700

    create darkmode by tail-wind

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -29,7 +29,7 @@ onUnmounted(() => {
 <template>
   <div class="relative">
     <div
-      class="fixed top-0 w-64 h-screen bg-white z-20 transform duration-300"
+      class="fixed top-0 w-64 h-screen bg-white dark:bg-gray-800 z-20 transform duration-300 dark:text-gray-300"
       :class="{ '-translate-x-full': !show }"
     >
       Side bar
@@ -40,16 +40,16 @@ onUnmounted(() => {
       v-show="show"
     ></div>
     <div
-      class="bg-gray-100 h-screen overflow-hidden duration-300"
+      class="bg-gray-100 dark:bg-gray-900 h-screen overflow-hidden duration-300"
       :class="{ 'xl:pl-64': show }"
     >
-      <div class="bg-white rounded shadow m-4 p-4">
+      <div class="bg-white dark:bg-gray-800 rounded shadow m-4 p-4">
         <MenuIcon
-          class="h-6 w-6 text-gray-600 cursor-pointer"
+          class="h-6 w-6 text-gray-600 dark:text-gray-300 cursor-pointer"
           @click="show = !show"
         />
       </div>
-      <div>
+      <div class="dark:text-gray-300">
         <slot />
       </div>
     </div>
